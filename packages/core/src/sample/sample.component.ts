72d030157bc4e952f76fc8d39da8fe69e04de0e6
Piotr Kosmowski <kospiotr@tlen.pl>
2018-01-18 15:45:30 +0100

    Refactoring

---
+++ packages/core/src/sample/sample.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'formify-sample',
+  templateUrl: './sample.component.html',
+  styleUrls: ['./sample.component.scss']
+})
+export class SampleComponent {
+}
---
+++ packages/core/src/sample/sample.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+import {SampleComponent} from "./sample.component";
+
+
+@NgModule({
+  declarations: [
+    SampleComponent
+  ],
+  imports: [],
+  exports: [SampleComponent],
+  providers: []
+})
+export class SampleModule { }
