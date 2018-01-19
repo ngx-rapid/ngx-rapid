0154081b73c132b089f49aca49ce0b81baf520dd
Piotr Kosmowski <kospiotr@tlen.pl>
2018-01-19 12:13:42 +0100

    Working example

--- packages/demo/src/@ngx-rapid/core/public_api.ts
+++
@@ -1,2 +0,0 @@
-export * from './src/sample.component';
-export * from './src/sample.module';
--- packages/demo/src/@ngx-rapid/core/src/sample.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'formify-sample',
-  templateUrl: './sample.component.html',
-  styleUrls: ['./sample.component.scss']
-})
-export class SampleComponent {
-}
