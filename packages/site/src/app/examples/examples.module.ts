eb07bb6fb28f1790163f655fe5a3cb8835615528
Piotr Kosmowski <kospiotr@tlen.pl>
2018-02-10 18:01:52 +0100

    Issue#5 Linting fix

--- packages/site/src/app/examples/examples.module.ts
+++ packages/site/src/app/examples/examples.module.ts
@@ -1,8 +1,8 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ExamplesComponent} from './examples.component';
-import {RouterModule} from "@angular/router";
-import {IndexQuickstartComponent} from "./index-quickstart.component";
+import {RouterModule} from '@angular/router';
+import {IndexQuickstartComponent} from './index-quickstart.component';
 
 @NgModule({
   imports: [
